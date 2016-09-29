ae6d50c535f1c915c3ad08ca3cd9a9cda439544c
antselley@gmail.com <antselley@gmail.com>
2016-09-29 15:01:01 +0200

    let's try again

--- functions_intro/getDay.js
+++ functions_intro/getDay.js
@@ -1,9 +1,8 @@
 function getDay (date) {
+  var d = Date(date);
   var weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Saturday','Sunday'] ;
   console.log(weekDays[date.getDay()])  ;
+  return weekDays[date.getDay()]
 }
 
 getDay('2016-09-17') ;
-
-
-Just testing... 
